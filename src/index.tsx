4c1b0eeb6510bd01010daa862d6bbb00fd4dd8cc
Hoony <59421544+kimyh03@users.noreply.github.com>
2020-06-26 12:39:34 +0900

    apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
+import { ApolloProvider } from "@apollo/react-hooks";
 import React from "react";
 import ReactDOM from "react-dom";
+import Client from "./Apollo/Client";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+ReactDOM.render(
+  <ApolloProvider client={Client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
