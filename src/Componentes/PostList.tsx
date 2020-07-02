1de95b71fe10f53f2cd784a25bca63cb4991c64a
Hoony <59421544+kimyh03@users.noreply.github.com>
2020-07-02 21:13:43 +0900

    category post list

---
+++ src/Componentes/PostList.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 10px 0;
+  border-bottom: ${(props) => props.theme.boxBorder};
+  :hover {
+    background: #fafafa;
+  }
+`;
+
+const Item = styled.div`
+  font-weight: 800;
+  font-size: 15px;
+  color: ${(props) => props.theme.carrotColor};
+  align-self: flex-start;
+  margin-left: 5px;
+`;
+
+const CoulmnItemSmall = styled.div`
+  opacity: 0.7;
+  text-align: center;
+  width: 67px;
+`;
+
+const CoulmnItemMiddle = styled.span`
+  opacity: 0.7;
+  text-align: center;
+  width: 143px;
+`;
+
+const CoulmnItemLarge = styled.div`
+  padding: 0 10px;
+  width: 461px;
+  opacity: 0.7;
+  display: flex;
+  align-items: center;
+`;
+
+interface IProps {
+  id: string;
+  userName: string;
+  title: string;
+  viewCount: string;
+  likeCount: string;
+  commentCount: string;
+  createdAt: string;
+}
+
+const PostList: React.FunctionComponent<IProps> = (props) => (
+  <Container>
+    <CoulmnItemSmall>{props.id}</CoulmnItemSmall>
+    <CoulmnItemLarge>
+      {props.title}
+      <Item>{`(${props.commentCount})`}</Item>
+    </CoulmnItemLarge>
+    {props.userName === "Hoony" ? (
+      <CoulmnItemMiddle>👑{props.userName}</CoulmnItemMiddle>
+    ) : (
+      <CoulmnItemMiddle>{props.userName}</CoulmnItemMiddle>
+    )}
+    <CoulmnItemMiddle>{props.createdAt.substring(0, 10)}</CoulmnItemMiddle>
+    <CoulmnItemSmall>{props.viewCount}</CoulmnItemSmall>
+    <CoulmnItemSmall>{props.likeCount}</CoulmnItemSmall>
+  </Container>
+);
+
+export default PostList;
--- src/Routes/Category/index.tsx
+++ src/Routes/Category/index.tsx
@@ -1,3 +1,147 @@
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 import React from "react";
+import { withRouter } from "react-router-dom";
+import Loader from "src/Componentes/Loader";
+import PostList from "src/Componentes/PostList";
+import styled from "styled-components";
 
-export default () => <span>Category</span>;
+interface Post {
+  id: string;
+  userName: string;
+  title: string;
+  viewCount: string;
+  likeCount: string;
+  commentCount: string;
+  createdAt: string;
+}
+
+interface Response {
+  ok: boolean;
+  error: string | null;
+  posts: Post[];
+}
+
+interface PostListData {
+  seePostList: Response;
+}
+
+interface SeePostListInput {
+  categoryTitle: string;
+  page: number;
+}
+
+const GET_POSTLIST = gql`
+  query seePostList($categoryTitle: String!, $page: Float!) {
+    seePostList(categoryTitle: $categoryTitle, page: $page) {
+      posts {
+        id
+        userName
+        title
+        viewCount
+        likeCount
+        commentCount
+        createdAt
+      }
+      ok
+      error
+    }
+  }
+`;
+
+const Container = styled.div`
+  background: white;
+  border: ${(props) => props.theme.boxBorder};
+  border-radius: 7px;
+  height: 100%;
+  max-height: 650px;
+`;
+
+const Coulmn = styled.div`
+  background: #fafafa;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  border-bottom: ${(props) => props.theme.boxBorder};
+`;
+
+const CoulmnItemSmall = styled.div`
+  opacity: 0.7;
+  text-align: center;
+  width: 8%;
+`;
+
+const CoulmnItemMiddle = styled.div`
+  opacity: 0.7;
+  text-align: center;
+  width: 17%;
+`;
+
+const CoulmnItemLarge = styled.div`
+  width: 55%;
+  text-align: center;
+  opacity: 0.7;
+`;
+
+const Title = styled.div`
+  font-size: 30px;
+  font-weight: 700;
+  margin: 10px 0;
+  color: ${(props) => props.theme.carrotColor};
+`;
+
+const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export default withRouter(
+  ({
+    match: {
+      params: { category, page }
+    }
+  }) => {
+    const PAGE = Number(page);
+    const { loading, data } = useQuery<PostListData, SeePostListInput>(
+      GET_POSTLIST,
+      {
+        variables: { categoryTitle: category, page: PAGE }
+      }
+    );
+    console.log(data);
+    return (
+      <>
+        <Title>{category}</Title>
+        <Container>
+          <Coulmn>
+            <CoulmnItemSmall>글번호</CoulmnItemSmall>
+            <CoulmnItemLarge>제목</CoulmnItemLarge>
+            <CoulmnItemMiddle>작성자</CoulmnItemMiddle>
+            <CoulmnItemMiddle>작성일</CoulmnItemMiddle>
+            <CoulmnItemSmall>조회수</CoulmnItemSmall>
+            <CoulmnItemSmall>추천수</CoulmnItemSmall>
+          </Coulmn>
+          <PostContainer>
+            {loading ? (
+              <Loader />
+            ) : (
+              data?.seePostList.posts.map((post) => (
+                <PostList
+                  key={post.id}
+                  id={post.id}
+                  userName={post.userName}
+                  title={post.title}
+                  viewCount={post.viewCount}
+                  likeCount={post.likeCount}
+                  commentCount={post.commentCount}
+                  createdAt={post.createdAt}
+                />
+              ))
+            )}
+          </PostContainer>
+        </Container>
+      </>
+    );
+  }
+);
